5acd5b2f565dc288ed2dd9eab2079c359d47613a
VishwajitNaik <44186372+VishwajitNaik@users.noreply.github.com>
2023-12-05 20:22:56 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,22 +1,56 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = (props) => {
+  const containerStyle = {
+    textAlign: 'center',
+    marginTop: '50px',
+  };
+
+  const buttonStyle = {
+    display: 'inline-block',
+    padding: '10px 20px',
+    margin: '0 10px',
+    textDecoration: 'none',
+    color: '#fff',
+    backgroundColor: '#007BFF',
+    borderRadius: '5px',
+    fontSize: '16px',
+    fontWeight: 'bold',
+    cursor: 'pointer',
+  };
+
+  const titleStyle = {
+    marginTop: '20px',
+    fontSize: '24px',
+  };
+
   return (
-    <div>
-        <div>
-            <h1>
-                <Link to="login">LogIn</Link>
-                <Link to="signup">SignUp</Link>
-            </h1>
-        </div>
-        <br />
+    <div style={containerStyle}>
+      <div>
+        <h1>
+          <Link to="/login" style={buttonStyle}>
+            Log In
+          </Link>
+          <Link to="/signup" style={buttonStyle}>
+            Sign Up
+          </Link>
+        </h1>
+      </div>
+      <br />
       <br />
       <br />
 
-      <h2>{props.name ? `Welcome - ${props.name}` : "Login please"}</h2>
+      <h2 style={titleStyle}>
+        {props.name ? `Welcome - ${props.name}` : "Please log in"}
+      </h2>
+
+      <div>
+        <h2 style={titleStyle}>Breaking News</h2>
+        {/* Include your breaking news content here */}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
